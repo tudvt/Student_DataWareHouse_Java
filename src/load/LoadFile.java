f274a56bdfd57036f41d6b9ff26ae691bfbd6d70
lexuanloi <lxl2808@gmail.com>
2020-08-02 23:56:35 +0700

    load file and upload to log

--- src/load/LoadFile.java
+++ src/load/LoadFile.java
@@ -1,27 +1,26 @@
-package data_warehouse;
+package load;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
-import com.chilkatsoft.CkGlobal;
-import com.chilkatsoft.CkScp;
-import com.chilkatsoft.CkSsh;
-
-import java.util.*;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class LoadFile {
+import com.chilkatsoft.CkGlobal;
+import com.chilkatsoft.CkScp;
+import com.chilkatsoft.CkSsh;
+
+public class test {
+	private static PreparedStatement pr = null;
+	private static ResultSet rs = null;
 
 	String condition;
 
@@ -37,92 +36,122 @@ public class LoadFile {
 			System.exit(1);
 		}
 	}
-
-	public static String scpDownload(String hostname, int port, String user, String pw, String remotePath,
-			String localPath) {
+//phuong thuc load file
+	public static void scpDownload(String hostname, int port, String user, String pw, String remotePath,
+			String localPath, String syn_must_math) {
 		CkSsh ssh = new CkSsh();
 		CkGlobal ck = new CkGlobal();
 		ck.UnlockBundle("hello ");
 		boolean success = ssh.Connect(hostname, port);
 		if (success != true) {
 			System.out.println(ssh.lastErrorText());
-			return "";
 		}
 		ssh.put_IdleTimeoutMs(5000);
 		success = ssh.AuthenticatePw(user, pw);
 		if (success != true) {
 			System.out.println(ssh.lastErrorText());
-			return "";
 		}
 		CkScp scp = new CkScp();
-
 		success = scp.UseSsh(ssh);
 		if (success != true) {
 			System.out.println(scp.lastErrorText());
-			return "";
 		}
-		// down tat ca cac file bat dau bang "sinhvien"
-		scp.put_SyncMustMatch("sinhvien_chieu*.*");
+		scp.put_SyncMustMatch(syn_must_math);
 		success = scp.SyncTreeDownload(remotePath, localPath, 2, false);
 		if (success != true) {
 			System.out.println(scp.lastErrorText());
-			return "";
 		}
-
 		ssh.Disconnect();
-		return "";
-
 	}
-
-	public static boolean log() {
-		boolean check = false;
-
+	
+// phuong thuc lay du lieu cua server va load file
+	public static boolean downloadFile() {
+		//tao bien tamp de kiem tra load thanh cong hay k
+		boolean tamp = false;
+		//cau lenh sql lay du lieu tu bang confgig
+		String sql = "SELECT * FROM config";
 		try {
-			// connect database
-			String myDriver = "com.mysql.jdbc.Driver";
-			String myUrl = "jdbc:mysql://localhost/wh";
-			Class.forName(myDriver);
-			Connection conn = DriverManager.getConnection(myUrl, "root", "");
-
-			// insert to table log
-			String sql = "INSERT INTO log (name,status,typeFile,path)" + "values(?,?,?,?);";
-			File dir = new File("D:\\DATA_WH\\text");
-			File[] children = dir.listFiles();
-			for (File file : children) {
-				PreparedStatement preparedStmt = conn.prepareStatement(sql);
-				// file name
-				preparedStmt.setString(1, file.getName());
-				// status
-				preparedStmt.setString(2, "ER");
-				// type file
-				if (file.getName().substring(file.getName().lastIndexOf(".")).equals(".xlsx")) {
-					preparedStmt.setString(3, "xlsx");
-				} else if (file.getName().substring(file.getName().lastIndexOf(".")).equals(".txt")) {
-					preparedStmt.setString(3, "txt");
-				} else if (file.getName().substring(file.getName().lastIndexOf(".")).equals(".csv")) {
-					preparedStmt.setString(3, "csv");
-				} else if (file.getName().substring(file.getName().lastIndexOf(".")).equals(".osheet")) {
-					preparedStmt.setString(3, "osheet");
-				} else {
-					preparedStmt.setString(3, "kb");
-				}
-				// file path
-				preparedStmt.setString(4, file.getAbsolutePath());
-
-				preparedStmt.executeUpdate();
-				check = true;
+			// connect voi db
+			pr = new ConnectDB().getConnection().prepareStatement(sql);
+			rs = pr.executeQuery();
+			while (rs.next()) {
+				// host name
+				String hostname = rs.getString("hostname");
+				//port
+				int port = rs.getInt("port");
+				//user name
+				String username = rs.getString("username");
+				//password
+				String pw = rs.getString("password");
+				//down tat ca cac file bat dau bang...
+				String syn_must_math = rs.getString("syn_must_math");
+				//duong dan cua server
+				String server_path = rs.getString("server_path");
+				//duong dan cua local
+				String local_path = rs.getString("local_path");
+				//phuong thuc load file
+				scpDownload(hostname, port, username, pw, server_path, local_path, syn_must_math);
+				//load thanh cong thi gan tamp=true
+				tamp = true;
+				return tamp;
 			}
-			conn.close();
+
 		} catch (Exception e) {
-			return check;
+			e.printStackTrace();
+			return tamp;
 		}
-		System.err.println("load to log success");
-		return check;
+		return tamp;
+	}
+	
+//phuong thuc ghi vao bang log
+	public static boolean log() {
+		int rs = 0;
+		//tao bien tamp de kiem tra ghi vao db thanh cong hay khong
+		boolean tamp = false;
+		
+		//cau sql ghi vao bang log trong db
+		String sql = "INSERT INTO log (name,status,typeFile,path)" + "values(?,?,?,?);";
+		File dir = new File("D:\\DATA_WH\\text");
+		//tao danh sach cac file vua tai ve
+		File[] file = dir.listFiles();
+		for (int i = 0; i < file.length; i++) {
+			try {
+				//connect voi db
+				pr = new ConnectDB().getConnection().prepareStatement(sql);
+				//ghi vao cot ten file
+				pr.setString(1, file[i].getName());
+				//ghi vao cot trang thai
+				pr.setString(2, "ER");
+				// loai file
+				if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".xlsx")) {
+					pr.setString(3, "xlsx");
+				} else if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".txt")) {
+					pr.setString(3, "txt");
+				} else if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".csv")) {
+					pr.setString(3, "csv");
+				} else if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".osheet")) {
+					pr.setString(3, "osheet");
+				} else {
+					pr.setString(3, "kb");
+				}
+				//ghi vao cot duong dan
+				pr.setString(4, file[i].getAbsolutePath());
+
+				rs = pr.executeUpdate();
+				//ghi thanh cong thi gan tamp=true
+				tamp = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return tamp;
+			}
+		}
+		return tamp;
 	}
 
-	public static boolean sendMail(String to, String subject, String bodyMail) {
+	//phuong thuc gui mai
+	public static boolean mail( String subject, String bodyMail) {
 		Properties props = new Properties();
-		// Setup mail server
+		// cau hinh mail server
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
@@ -133,21 +162,17 @@ public class LoadFile {
 				return new PasswordAuthentication("lle718817@gmail.com", "tes0335444964");
 			}
 		});
-		// Used to debug SMTP issues
-//		session.setDebug(true);
 		try {
-			// Create a default MimeMessage object.
 			MimeMessage message = new MimeMessage(session);
-			// Set From: header field of the header.
 			message.setHeader("Content-Type", "text/plain; charset=UTF-8");
-			// Set To: header field of the header.
+			//dia chi mail gui di
 			message.setFrom(new InternetAddress("lle718817@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-			// Set Subject: header field
+			//dia chi mai gui den
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("lxl2808@gmail.com"));
+			//tieu de mail
 			message.setSubject(subject, "UTF-8");
-			// Now set the actual message
+			//noi dung mail
 			message.setText(bodyMail, "UTF-8");
-			// Send message
 			Transport.send(message);
 		} catch (MessagingException e) {
 			e.printStackTrace();
@@ -156,28 +181,22 @@ public class LoadFile {
 		return true;
 	}
 
-	public static void sendMailLog() {
+	public static void sendMail() {
+		//neu log tra ve false thi gui mail bao khong thanh cong
 		if (log() != true) {
-			sendMail("17130113@st.hcmuaf.edu.vn", "DATA WAREHOUSE - 2020", "Download file khong thanh cong! ");
-			System.out.println("Send Email- fail...!");
-		} else {
-			sendMail("17130113@st.hcmuaf.edu.vn", "DATA WAREHOUSE - 2020", "Downoad file thanh cong! ");
-			System.out.println("Send Email- success...!");
+			mail("data warehouses 2020", "ghi vao log khong thanh cong ");
+			System.out.println("khong thanh cong");
+		}
+		//neu log tra ve true thi gui mail bao thanh cong
+		else {
+			mail("data warehouses 2020", "ghi vao log thanh cong");
+			System.out.println("thanh cong");
 		}
 	}
 
 	public static void main(String argv[]) {
-		String hostname = "drive.ecepvn.org";
-		int port = 2227;
-		String user = "guest_access";
-		String pw = "123456";
-		// souce
-		String remotePath = "/volume1/ECEP/song.nguyen/DW_2020/data";
-		// thu muc muon down file ve
-		String localPath = "D:\\DATA_WH\\text";
-//		scpDownload(hostname, port, user, pw, remotePath, localPath);
-		log();
-		sendMailLog();
+		downloadFile();
+		sendMail();
 	}
 
 }
