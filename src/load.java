d043f1d2203958971a064214fe01ee862dd3b8f7
tuongtu <dotuongtu196@gmail.com>
2020-08-09 15:10:36 +0700

    load v3

--- src/load.java
+++ src/load.java
@@ -1,7 +1,8 @@
-package src;
+import java.io.File;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,25 +17,31 @@ import com.chilkatsoft.CkSsh;
 
 public class load {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/datacontrol";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/datacontrol";
+
 	// Ten nguoi dung va mat khau cua co so du lieu
 	static final String USER = "root";
-	static final String PASS = ""; 
+	static final String PASS = "123456";
 	static Connection conn = null;
 	static CallableStatement stmt = null;
 	static ArrayList<String> listER = new ArrayList<String>();
 	static Map<String, String> map = new HashMap<>();
 	static SendEmail sendMail;
-	private static String USER_NAME = "dotuongtu197@gmail.com"; //ng gui
+	private static String USER_NAME = "dotuongtu197@gmail.com"; // GMail user
+																// name (just
+																// the part
+																// before
+																// "@gmail.com")
 	private static String PASSWORD = "kid159753"; // GMail password
-	private static String RECIPIENT = "dotuongtu198@gmail.com";//ng nhan
+	private static String RECIPIENT = "dotuongtu198@gmail.com";
 	static String subject = "Thong bao ";
-	static String body = "load to stagging thanh cong";
+	static String body = " thanh cong";
 	static String[] listEmail = { RECIPIENT };
 	static String table_target = "";
 	static String db_target = "";
 	static String temp_target = "";
 	static String db_config = "";
+	static String table_end = "";
 
 	static void connectDb() throws ClassNotFoundException, SQLException {
 
@@ -55,13 +62,57 @@ public class load {
 		ResultSet rs = stmt.executeQuery();
 		while (rs.next()) {
 			db_target = rs.getString(12);
-			System.err.println(db_target);
-			table_target = rs.getString(13);
-			temp_target = rs.getString(14);
+			System.out.println(db_target);
+			table_target = rs.getString(13);// table stagiing
+			temp_target = rs.getString(14);// table temp
 			db_config = rs.getString(15);
+			table_end = rs.getString(16);// table sau khi da edit temp
 		}
+		
+		
 	}
-
+	public static boolean log() {
+		int rs = 0;
+		//tao bien tamp de kiem tra ghi vao db thanh cong hay khong
+		boolean tamp = false;
+		PreparedStatement pr = null;
+		//cau sql ghi vao bang log trong db
+		String sql = "INSERT INTO log(filenamesrc,statusend,typeFile)" + "values(?,?,?);";
+		File dir = new File("C:\\Users\\Tuong Tu\\Desktop\\local");
+		//tao danh sach cac file vua tai ve
+		File[] file = dir.listFiles();
+		for (int i = 0; i < file.length; i++) {
+			try {
+				//connect voi db
+				pr = conn.prepareCall(sql);
+				//ghi vao cot duong dan
+				 pr.setString(1, file[i].getAbsolutePath());
+				//ghi vao cot trang thai
+				pr.setString(2, "ER1");
+				// loai file
+				if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".xlsx")) {
+					pr.setString(3, "xlsx");
+				} else if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".txt")) {
+					pr.setString(3, "txt");
+				} else if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".csv")) {
+					pr.setString(3, "csv");
+				} else if (file[i].getName().substring(file[i].getName().lastIndexOf(".")).equals(".osheet")) {
+					pr.setString(3, "osheet");
+				} else {
+					pr.setString(3, "kb");
+				}
+			
+			
+				rs = pr.executeUpdate();
+				//ghi thanh cong thi gan tamp=true
+				tamp = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return tamp;
+			}
+		}
+		return tamp;
+	}
 	private void getFileFromServer() throws SQLException {
 
 		// lay data ve
@@ -127,18 +178,20 @@ public class load {
 	}
 
 	static void loadToStagging() throws SQLException {
+		// su dung db sinh vien
 		String useDB = "use " + db_target;
 		System.out.println(useDB);
 
 		System.out.println(table_target);
 		stmt.executeUpdate(useDB);
-		System.err.println("Bat dau load");
+		System.out.println("Bat dau load");
+		// duyet cai map lay ra các file ER o phan tren
 		for (Map.Entry<String, String> entry : map.entrySet()) {
 			String target = "sinhvien.stagging";
 			String k = entry.getKey();
 			String v = entry.getValue();
 			System.out.println("Key: " + k + ", Value: " + v);
-
+			// load file vao db
 			// dung load file ko the bo vao procedure
 			String load_stagging = "LOAD DATA  INFILE '" + v + "' " + "INTO TABLE " + target + ""
 					+ " FIELDS TERMINATED BY '\t' " + "ENCLOSED BY '' " + "LINES TERMINATED BY '\r\n';";
@@ -147,29 +200,32 @@ public class load {
 			stmt.executeUpdate(load_stagging);
 			System.out.println("load ok");
 			String sql_filename = "";
-// cai do la cai gi v z. duong dan em khac ma
+
 		}
-sendMail = new SendEmail();
-		 sendMail.sendFromGMail(USER_NAME, PASSWORD, listEmail, subject,
-		 body);
-		System.err.println("gui email ok");
+		sendMail = new SendEmail();
+		sendMail.sendFromGMail(USER_NAME, PASSWORD, listEmail, subject, body);
+		System.out.println("gui email ok");
 	}
 
 	private static void loadToTemp() throws SQLException {
 		String use_dc = "use " + db_target;
 		stmt.executeUpdate(use_dc);
+		// goi ham insert gai tri tu stagging vao temp
 		String call_insert = " insert into " + temp_target + " select * from " + table_target + "";
+		
 		stmt = conn.prepareCall(call_insert);
 		stmt.executeUpdate();
-		System.err.println("load from stagging to temp");
+		System.out.println("load from stagging to temp");
 
 		System.out.println("dung db sinh vien");
 		String call_truncate = " TRUNCATE TABLE " + table_target + ";";
 		stmt = conn.prepareCall(call_truncate);
 		System.out.println("xoa du lieu trong stagging");
 		stmt.executeUpdate();
-		System.err.println("load from stagging to temp");
 
+		 sendMail = new SendEmail();
+		 sendMail.sendFromGMail(USER_NAME, PASSWORD, listEmail, subject,
+		 body);
 	}
 
 	private static void editTemp() throws SQLException {
@@ -183,7 +239,7 @@ sendMail = new SendEmail();
 		stmt.executeUpdate(deleteStt);
 		System.out.println("xoa dong stt");
 		// them sk vao ne
-		String alter_temp = " ALTER TABLE " + temp_target + " ADD sk INT PRIMARY KEY AUTO_INCREMENT;";
+		String alter_temp = " ALTER TABLE " + temp_target + " ADD sk INT ";
 		stmt = conn.prepareCall(alter_temp);
 		stmt.executeUpdate();
 		System.out.println("tao khoa sk cho temp");
@@ -192,7 +248,7 @@ sendMail = new SendEmail();
 		stmt = conn.prepareCall(addDate_temp);
 		stmt.executeUpdate();
 		System.out.println("them cot date_temp");
-		// day la date sk láº¥y tá»« date dim
+		// day la date sk lấy từ date dim
 		String addDate_sk = "alter table " + temp_target + " add date_sk int;";
 		stmt = conn.prepareCall(addDate_sk);
 		stmt.executeUpdate();
@@ -202,7 +258,7 @@ sendMail = new SendEmail();
 		stmt = conn.prepareCall(addDate_lastchange);
 		stmt.executeUpdate();
 		System.out.println("them cot date_lastchange");
-		// set gia tri cot lÃ  ngay hien tai
+		// set gia tri cot là ngay hien tai
 		String add_curdate = "UPDATE " + temp_target + " SET date_temp = curdate(); ";
 		stmt = conn.prepareCall(add_curdate);
 		stmt.executeUpdate();
@@ -214,18 +270,53 @@ sendMail = new SendEmail();
 		stmt.executeUpdate();
 		System.out.println("update date sk cho table");
 
+		String table_end_sql =  "insert into " + table_end
+				+ "(stt,ho_lot,ten,masv,ngay_sinh,malop,tenlop,dt,email,quequan,ghichu,date_temp,date_sk,date_lastchange)"
+				+ " select stt,ho_lot,ten,masv,ngay_sinh,malop,tenlop,dt,email,quequan,ghichu,date_temp,date_sk,date_lastchange from "
+				+ temp_target + "";
+		stmt = conn.prepareCall(table_end_sql);
+		stmt.executeUpdate();
+		System.out.println("ra table cuoi cung");
+		// delete tat ca may cot them vao temp
+		String delete_sk = "ALTER TABLE " + temp_target + " DROP COLUMN sk;";
+		stmt = conn.prepareCall(delete_sk);
+		stmt.execute();
+
+		String delete_date_lastchange = "ALTER TABLE " + temp_target + " DROP COLUMN date_lastchange;";
+		stmt = conn.prepareCall(delete_date_lastchange);
+		stmt.execute();
+
+		String delete_date_temp = "ALTER TABLE " + temp_target + " DROP COLUMN date_temp;";
+		stmt = conn.prepareCall(delete_date_temp);
+		stmt.execute();
+
+		String delete_date_sk = "ALTER TABLE " + temp_target + " DROP COLUMN date_sk;";
+		stmt = conn.prepareCall(delete_date_sk);
+		stmt.execute();
+
+		String truncate_temp = "TRUNCATE TABLE " + temp_target + ";";
+		stmt = conn.prepareCall(truncate_temp);
+		stmt.executeUpdate();
+
+		System.out.println("delete all new add col");
+
+		 sendMail = new SendEmail();
+		 sendMail.sendFromGMail(USER_NAME, PASSWORD, listEmail, subject,
+		 body);
+		System.out.println("gui mail ok ");
 	}
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
-		// int id_config = Integer.parseInt(args[0]);
-
 		connectDb();
-		getConfigData(1);
-		getFileER(1);
+		int id_config = Integer.parseInt(args[0]);
+
+		getConfigData(id_config);
+		//log();
+		getFileER(id_config);
 		loadToStagging();
-		//loadToTemp();
-		//editTemp();
+		loadToTemp();
+		editTemp();
+		System.out.println("okkkkkk");
 
 	}
 }
